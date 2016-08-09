74718ab197bc640621456bad4723d5c4b83c4fac
1-0 <1_0@usa.com>
2016-08-09 14:52:12 +0300

    start timerulex

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # timerulex
+Fast to run and cross-platform  time rule set gui. Based on PySide.
+
---
+++ timerulex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''timerulex - time rules gui, based on PySide writed on Python. Licensed by GPL3.'''
+
+import sys
+import timerulex
+from timerulex.main import *
+
+
+if __name__ == "__main__":
+    timerulex.main.main(sys.argv)
+    sys.exit()
+
